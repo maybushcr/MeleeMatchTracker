0f8a3672b48f1502e1a78764d5005dde033c409d
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-11-14 16:05:36 -0500

    inital commit of starter Spring project

---
+++ melee-tracker/src/main/java/com/vcu/meleetracker/MeleeTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.vcu.meleetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeleeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeleeTrackerApplication.class, args);
+	}
+}
