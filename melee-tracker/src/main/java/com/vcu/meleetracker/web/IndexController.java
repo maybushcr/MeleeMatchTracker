f2cc6079596c39d74561eae28c53569d0412bd40
Ryan Maybush <maybushcr@mymail.vcu.edu>
2018-11-29 13:41:50 -0500

    Added HTML files for the other tables and nav links at top of pages

---
+++ melee-tracker/src/main/java/com/vcu/meleetracker/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.vcu.meleetracker.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+
+
+}
