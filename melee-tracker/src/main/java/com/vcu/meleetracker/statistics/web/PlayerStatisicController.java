d992ba869988c494e1579f0034526fecbae7d0bc
Ryan Maybush <maybushcr@mymail.vcu.edu>
2018-12-02 22:24:59 -0500

    Added stats to players

--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/PlayerStatisicController.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/PlayerStatisicController.java
@@ -1,7 +1,11 @@
 package com.vcu.meleetracker.statistics.web;
 
+import com.vcu.meleetracker.database.repo.PlayerRepository;
+import com.vcu.meleetracker.statistics.dao.PVPWinRatesDao;
+import com.vcu.meleetracker.statistics.dao.PopularThrowsDao;
 import com.vcu.meleetracker.statistics.dao.AverageStockDifferenceDao;
 import com.vcu.meleetracker.statistics.dto.AverageStockDifference;
+import com.vcu.meleetracker.statistics.dto.PVPWinRates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +18,16 @@ public class PlayerStatisicController {
 
     @Autowired
     AverageStockDifferenceDao averageStockDifferenceDao;
+    @Autowired
+    PVPWinRatesDao pvpWinRatesDao;
+    @Autowired
+    PopularThrowsDao popularThrowsDao;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getPlayerStatistics(Model model){
         model.addAttribute("average_stock_difference",averageStockDifferenceDao.findAll());
+        model.addAttribute("popular_throws",popularThrowsDao.findAll());
+        model.addAttribute("pvp_win_rates",pvpWinRatesDao.findAll());
         return "statistics/players";
     }
 
