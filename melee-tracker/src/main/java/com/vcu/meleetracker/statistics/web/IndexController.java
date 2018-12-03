9d0a1145f41c4d7cc04a952c70dd812efaa215ba
Ryan Hayden <ryanhayden@live.com>
2018-12-02 22:34:05 -0500

    got rid of unused parts of index controller

--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/IndexController.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/IndexController.java
@@ -1,9 +1,6 @@
 package com.vcu.meleetracker.statistics.web;
 
 
-import com.vcu.meleetracker.statistics.dao.PlayerWinRateByPlayerDao;
-import com.vcu.meleetracker.statistics.dao.PopularThrowsDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class IndexController {
 
-    @Autowired
-    PopularThrowsDao popularThrowsDao;
-    @Autowired
-    PlayerWinRateByPlayerDao playerWinRateByPlayerDao;
-
     @RequestMapping(method = RequestMethod.GET)
     public String getPlayers(Model model){
-        model.addAttribute("popular_throws",popularThrowsDao.findAll());
-        model.addAttribute("player_win_rate_by_player",playerWinRateByPlayerDao.findAll());
         return "index";
     }
 }
