177aca86ba94eea866b77f77a67a4791b690079e
Ryan Hayden <ryanhayden@live.com>
2018-12-03 00:08:37 -0500

    added mapping

--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/MiscellaneousStatisticController.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/web/MiscellaneousStatisticController.java
@@ -2,10 +2,13 @@ package com.vcu.meleetracker.statistics.web;
 
 import com.vcu.meleetracker.statistics.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping(value = "/statistics/miscellaneous")
 public class MiscellaneousStatisticController {
 
     @Autowired
