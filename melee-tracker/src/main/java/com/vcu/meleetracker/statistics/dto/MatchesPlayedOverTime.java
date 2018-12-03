66b28847c24ffe22574418fc800abc3ebcabccd3
Ryan Hayden <ryanhayden@live.com>
2018-12-02 21:43:08 -0500

    more dto (:

--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dto/MatchesPlayedOverTime.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dto/MatchesPlayedOverTime.java
@@ -1,8 +1,47 @@
 package com.vcu.meleetracker.statistics.dto;
 
+import java.sql.Date;
 import java.util.Objects;
 
 public class MatchesPlayedOverTime {
 
+    private Date day;
+    private Integer count;
 
+    public MatchesPlayedOverTime(){}
+
+    public MatchesPlayedOverTime(Date day, Integer count) {
+        this.day = day;
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MatchesPlayedOverTime that = (MatchesPlayedOverTime) o;
+        return Objects.equals(day, that.day) &&
+                Objects.equals(count, that.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, count);
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }
--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dto/TournamentTypeFrequency.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dto/TournamentTypeFrequency.java
@@ -1,4 +1,46 @@
 package com.vcu.meleetracker.statistics.dto;
 
+import java.util.Objects;
+
 public class TournamentTypeFrequency {
+
+    private Integer tournament_type_id;
+    private Integer count;
+
+    public TournamentTypeFrequency(){}
+
+    public TournamentTypeFrequency(Integer tournament_type_id, Integer count) {
+        this.tournament_type_id = tournament_type_id;
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TournamentTypeFrequency that = (TournamentTypeFrequency) o;
+        return Objects.equals(tournament_type_id, that.tournament_type_id) &&
+                Objects.equals(count, that.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tournament_type_id, count);
+    }
+
+    public Integer getTournament_type_id() {
+        return tournament_type_id;
+    }
+
+    public void setTournament_type_id(Integer tournament_type_id) {
+        this.tournament_type_id = tournament_type_id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }
