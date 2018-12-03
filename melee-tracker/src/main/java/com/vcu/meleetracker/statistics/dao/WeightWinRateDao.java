4504a9bffeb2a7476907de12d6420fdd2e575b01
Ryan Hayden <ryanhayden@live.com>
2018-12-02 20:39:59 -0500

    added right query

--- melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dao/WeightWinRateDao.java
+++ melee-tracker/src/main/java/com/vcu/meleetracker/statistics/dao/WeightWinRateDao.java
@@ -21,18 +21,43 @@ public class WeightWinRateDao {
     }
 
     public List<WeightWinRate> findAll() {
-        return this.jdbcTemplate.query( "select throw_name, sum(cnt) as count\n" +
+        return this.jdbcTemplate.query( "select character_weight, IFNULL(sum(wins)/sum(games_played),0) as win_percentage\n" +
                 "from\n" +
                 "(\n" +
-                "  (select player1_throw as throw_name, count(*) as cnt\n" +
-                "  from rock_paper_scissors\n" +
-                "  group by player1_throw) \n" +
-                "UNION ALL\n" +
-                "  (select player2_throw as throw_name, count(*) as cnt\n" +
-                "  from rock_paper_scissors\n" +
-                "  group by player2_throw)\n" +
-                ") all_throws\n" +
-                "group by throw_name;", new WeightWinRateDao.WeightWinRateMapper());
+                "  select participant, sum(games) as games_played\n" +
+                "  from\n" +
+                "  (\n" +
+                "    (select player1_character_id as participant, count(*) as games\n" +
+                "    from matches\n" +
+                "    group by player1_character_id) \n" +
+                "  UNION ALL\n" +
+                "    (select player2_character_id as participant, count(*) as games\n" +
+                "    from matches\n" +
+                "    group by player2_character_id) \n" +
+                "  ) all_games\n" +
+                "  group by participant\n" +
+                ") grouped_games\n" +
+                "LEFT OUTER JOIN\n" +
+                "(\n" +
+                "  select winner, sum(wins) as wins\n" +
+                "  from\n" +
+                "  (\n" +
+                "    (select player1_character_id as winner, count(*) as wins\n" +
+                "    from matches\n" +
+                "    where player1_stocks > player2_stocks\n" +
+                "    group by player1_character_id) \n" +
+                "  UNION ALL\n" +
+                "    (select player2_character_id as winner, count(*) as wins\n" +
+                "    from matches\n" +
+                "    where player2_stocks > player1_stocks\n" +
+                "    group by player2_character_id) \n" +
+                "  ) all_wins\n" +
+                "  group by winner\n" +
+                ") grouped_wins\n" +
+                "ON participant = winner\n" +
+                "JOIN characters\n" +
+                "ON participant = character_id\n" +
+                "group by character_weight;", new WeightWinRateDao.WeightWinRateMapper());
     }
 
     private static final class WeightWinRateMapper implements RowMapper<WeightWinRate> {
